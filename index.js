78401bfcd24677ba7dc16a9e62b59e095ca6f437
Marius Berget <kaizokupuffball@gmail.com>
2020-12-27 21:48:13 +0100

    Update index.js
    added audio tag "dubbed"
    fixed issue in type with SCR not beeing wordbound
    fixed issue when folder names has "nordic.eng"

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ export default function(str) {
     resolution: /\d{3,4}p/i,
     size:       /\d{2,5}[MG]B/i,
     color:      /(10-bit|\bHDR\b)/i,
-    tags:       /(COMPLETE|LIMITED|INTERNAL)/i,
+    tags:       /(COMPLETE|LIMITED|INTERNAL|DUBBED)/i,
     video:      /(REMUX?[\.\s](AVC|HEVC)?)|((AVC|HEVC)?[\.\s]?REMUX)|NTSC|PAL|[xh]?[\.\s]?(264|265)/i,
     release:    /REAL[\.\s]PROPER|PROPER|REPACK|READNFO|READ[\.\s]NFO|DiRFiX|NFOFiX/i,
-    lang:       /MULTISUBS|MULTI|NORDIC|ENG|DANISH|SWEDISH|NORWEGIAN|GERMAN|ITALIAN|FRENCH|SPANISH/i,
+    lang:       /MULTISUBS|MULTI|NORDIC?([\.\s]ENG)?|DANISH|SWEDISH|NORWEGIAN|GERMAN|ITALIAN|FRENCH|SPANISH/i,
     edition:    /UNRATED|DC|REMASTERED|(Directors|EXTENDED)[\.\s](CUT|EDITION)|EXTENDED|3D|2D|\bNF\b|\bAMZN\b/i,
-    type:       /CAM|\bTS(?!C)\b|TELESYNC|(DVD|BD)SCR|SCR|DDC|R5[\.\s]LINE|R5|(DVD|HD|BR|BD|WEB)Rip|DVD|DVDR|(HD|PD)TV|WEB-DL|WEBDL|BluRay|WEB/i,
+    type:       /CAM|\bTS(?!C)\b|TELESYNC|(DVD|BD)SCR|\bSCR\b|DDC|R5[\.\s]LINE|R5|(DVD|HD|BR|BD|WEB)Rip|DVD|DVDR|(HD|PD)TV|WEB-DL|WEBDL|BluRay|WEB/i,
     audio:      /AAC2[\.\s]0|AAC|(DTS-HD[\.\s]MA[\.\s]5[\.\s]1|DTS[0-9][\.\s][0-1]|DTS)|(AC3[\.\s](DD2[\.\s]0|DD5[\.\s]1)|AC3|DD[\+]?Atmos?5[\.\s]1|DD5[\.\s]1)|(DDP5[\.\s]1)|(TRUEHD[\.\s]5[\.\s]1)/i,
-    other:      /UHD|DOCU/i,    
+    other:      /UHD|DOCU/i,
     group:      /\w+$/i
   };
   
